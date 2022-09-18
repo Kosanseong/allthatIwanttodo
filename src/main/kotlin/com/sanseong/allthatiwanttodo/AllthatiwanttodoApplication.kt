acbbe5bbf9959e78760f00d455e106a80b3df71f
sanseongko <tkstjd565@naver.com>
2022-09-19 03:43:07 +0900

    first commit

---
+++ src/main/kotlin/com/sanseong/allthatiwanttodo/AllthatiwanttodoApplication.kt
@@ -0,0 +1,11 @@
+package com.sanseong.allthatiwanttodo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AllthatiwanttodoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AllthatiwanttodoApplication>(*args)
+}
---
+++ src/test/kotlin/com/sanseong/allthatiwanttodo/AllthatiwanttodoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sanseong.allthatiwanttodo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AllthatiwanttodoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
