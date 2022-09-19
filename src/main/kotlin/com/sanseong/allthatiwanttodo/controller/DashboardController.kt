345aee7b2e5859a3a8117c59d1debbdfaa054aa0
sanseongko <tkstjd565@naver.com>
2022-09-20 02:23:57 +0900

    게시판 단일 조회 컨트롤러 추가

--- src/main/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardController.kt
+++ src/main/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardController.kt
@@ -2,7 +2,10 @@ package com.sanseong.allthatiwanttodo.controller
 
 import com.sanseong.allthatiwanttodo.model.dashboard.request.DashboardCreateRequest
 import com.sanseong.allthatiwanttodo.model.dashboard.response.DashboardCreateResponse
+import com.sanseong.allthatiwanttodo.model.dashboard.response.DashboardGetResponse
 import com.sanseong.allthatiwanttodo.service.DashboardService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,8 +21,13 @@ class DashboardController(
     fun writeDashboard(
         @RequestBody request: DashboardCreateRequest,
     ): DashboardCreateResponse {
-        val dashboardId = dashboardService.writeBoard(request.toModel())
+        return DashboardCreateResponse(dashboardService.writeBoard(request.toModel()).id)
+    }
 
-        return DashboardCreateResponse(dashboardId)
+    @GetMapping("/{dashboardId}")
+    fun getDashboard(
+        @PathVariable dashboardId: Long,
+    ): DashboardGetResponse {
+        return DashboardGetResponse.createBy(dashboardService.getBoard(dashboardId))
     }
 }
