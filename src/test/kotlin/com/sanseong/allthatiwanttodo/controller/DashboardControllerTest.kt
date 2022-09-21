82889d8bc4cf04571e110bcd403d6c960d5a2a99
sanseongko <tkstjd565@naver.com>
2022-09-21 14:10:21 +0900

    테스트 given 밸류 수정 및 단일 조회 컨트롤러 테스트 코드 추가

--- src/test/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardControllerTest.kt
+++ src/test/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardControllerTest.kt
@@ -4,11 +4,15 @@ import com.sanseong.allthatiwanttodo.entity.Dashboard
 import com.sanseong.allthatiwanttodo.service.DashboardService
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
 
 internal class DashboardControllerTest {
     private var dashboardService: DashboardService = mockk()
@@ -22,8 +26,8 @@ internal class DashboardControllerTest {
         //given
         val dashboard = Dashboard(
             id = 1,
-            title = "테스트 컨텐트",
-            content = "테스트 타이틀",
+            title = "테스트 타이틀",
+            content = "테스트 컨텐트",
         )
 
         every { dashboardService.writeBoard(any()) } returns dashboard
@@ -44,5 +48,28 @@ internal class DashboardControllerTest {
             content { contentType(MediaType.APPLICATION_JSON) }
             content { json("""{"dashboardId": 1}""") }
         }
+    }
+
+    @Test
+    fun getBoard() {
+        //given
+        val dashboard = Dashboard(
+            id = 1,
+            title = "테스트 타이틀",
+            content = "테스트 컨텐트",
+        )
+
+        every { dashboardService.getBoard(1) } returns dashboard
+
+        val queryParam =
+        //when
+        //then
+        mockMvc.get("/dashboard/1")
+            .andExpect {
+                content { contentType(MediaType.APPLICATION_JSON) }
+                jsonPath("$.dashboardId") { value(1) }
+                jsonPath("$.title") { value("테스트 타이틀") }
+                jsonPath("$.content") {value("테스트 컨텐트")}
+            }
     }
 }
