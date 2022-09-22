20d70ea2b839555a8f4b5289b73b4c82224ac79e
sanseongko <tkstjd565@naver.com>
2022-09-22 23:35:19 +0900

    게시판 페이지 컨트롤러 추가

--- src/main/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardController.kt
+++ src/main/kotlin/com/sanseong/allthatiwanttodo/controller/DashboardController.kt
@@ -2,13 +2,17 @@ package com.sanseong.allthatiwanttodo.controller
 
 import com.sanseong.allthatiwanttodo.model.dashboard.request.DashboardCreateRequest
 import com.sanseong.allthatiwanttodo.model.dashboard.response.DashboardCreateResponse
+import com.sanseong.allthatiwanttodo.model.dashboard.response.DashboardGetPageResponse
 import com.sanseong.allthatiwanttodo.model.dashboard.response.DashboardGetResponse
 import com.sanseong.allthatiwanttodo.service.DashboardService
+import kotlin.streams.toList
+import org.springframework.data.domain.PageRequest
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -29,5 +33,15 @@ class DashboardController(
         @PathVariable id: Long,
     ): DashboardGetResponse {
         return DashboardGetResponse.createBy(dashboardService.getBoard(id))
+    }
+
+    @GetMapping("/page")
+    fun getDashboardPage(
+        @RequestParam page: Int?,
+        @RequestParam size: Int?,
+    ): DashboardGetPageResponse {
+        val pageable = PageRequest.of(page ?: 0, size ?: 10)
+
+        return DashboardGetPageResponse.createBy(dashboardService.getBoardPage(pageable))
     }
 }
